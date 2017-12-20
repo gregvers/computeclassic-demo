c7b59f8ab07459f4bfcf1a061c100b8b386760af
gregvers <gregvers@gmail.com>
2017-12-20 14:01:40 -0800

    update

--- 04_upload-orchestrations_v1.sh
+++ 04_upload-orchestrations_v1.sh
@@ -5,57 +5,57 @@
 
 # Upload or update orchestrations
 # commondefs update
-if [[ "$(opc compute orchestrations list /Compute-$IDENTITYDOMAIN/$USER/commondefs)" != *"\"result\": []"* ]] ;
+if [[ "$(opc compute orchestrations list /Compute-$IDENTITYDOMAIN/$USER/common)" != *"\"result\": []"* ]] ;
 then
-	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/commondefs --request-body=./v1/commondefs_v1.json
+	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/common --request-body=./v1/common.json
 else
-	opc compute orchestrations add --request-body=./v1/common_v1.json
+	opc compute orchestrations add --request-body=./v1/common.json
 fi
 
 # natgw update
 if [[ "$(opc compute orchestrations list /Compute-$IDENTITYDOMAIN/$USER/natgw)" != *"\"result\": []"* ]] ;
 then
-	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/natgw --request-body=./v1/natgw_v1.json
+	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/natgw --request-body=./v1/natgw.json
 else
-	opc compute orchestrations add --request-body=./v1/natgw_v1.json
+	opc compute orchestrations add --request-body=./v1/natgw.json
 fi
 
 # jumphost update
 if [[ "$(opc compute orchestrations list /Compute-$IDENTITYDOMAIN/$USER/jumphost)" != *"\"result\": []"* ]] ;
 then
-	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/jumphost --request-body=./v1/jumphost_v1.json
+	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/jumphost --request-body=./v1/jumphost.json
 else
-	opc compute orchestrations add --request-body=./v1/jumphost_v1.json
+	opc compute orchestrations add --request-body=./v1/jumphost.json
 fi
 
 # lbr update
 if [[ "$(opc compute orchestrations list /Compute-$IDENTITYDOMAIN/$USER/lbr)" != *"\"result\": []"* ]] ;
 then
-	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/lbr --request-body=./v1/lbr_v1.json
+	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/lbr --request-body=./v1/lbr.json
 else
-	opc compute orchestrations add --request-body=./v1/lbr_v1.json
+	opc compute orchestrations add --request-body=./v1/lbr.json
 fi
 
 # apps update
 if [[ "$(opc compute orchestrations list /Compute-$IDENTITYDOMAIN/$USER/apps)" != *"\"result\": []"* ]] ;
 then
-	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/apps --request-body=./v1/apps_v1.json
+	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/apps --request-body=./v1/apps.json
 else
-	opc compute orchestrations add --request-body=./v1/apps_v1.json
+	opc compute orchestrations add --request-body=./v1/apps.json
 fi
 
 # db update
 if [[ "$(opc compute orchestrations list /Compute-$IDENTITYDOMAIN/$USER/db)" != *"\"result\": []"* ]] ;
 then
-	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/db --request-body=./v1/db_v1.json
+	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/db --request-body=./v1/db.json
 else
-	opc compute orchestrations add --request-body=./v1/db_v1.json
+	opc compute orchestrations add --request-body=./v1/db.json
 fi
 
 # master update
 if [[ "$(opc compute orchestrations list /Compute-$IDENTITYDOMAIN/$USER/master)" != *"\"result\": []"* ]] ;
 then
-	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/master --request-body=./v1/master_v1.json
+	opc compute orchestrations update /Compute-$IDENTITYDOMAIN/$USER/master --request-body=./v1/master.json
 else
-	opc compute orchestrations add --request-body=./v1/master_v1.json
+	opc compute orchestrations add --request-body=./v1/master.json
 fi
--- env.sh
+++ env.sh
@@ -3,16 +3,20 @@
 # endpoint, account and user
 export IDENTITYDOMAIN=computepm
 export USER=gregory.verstraeten@oracle.com
-export PASSWORD="9W&VUBGBcG"
+export PASSWORD="PM4UenrCYs"
+
+# sitec/computepm
 export COMPUTE_ENDPOINT=http://10.252.131.38:7777
-#export STORAGE_AUTH_ENDPOINT=http://$IDENTITYDOMAIN.devpool0.opcstorage.com:7820/auth/v1.0
-#export STORAGE_ENDPOINT=http://$IDENTITYDOMAIN.devpool0.opcstorage.com:7820/v1/Storage-$IDENTITYDOMAIN
+export STORAGE_AUTH_ENDPOINT=http://$IDENTITYDOMAIN.devpool0.opcstorage.com:7820/auth/v1.0
+export STORAGE_ENDPOINT=http://$IDENTITYDOMAIN.devpool0.opcstorage.com:7820/v1/Storage-$IDENTITYDOMAIN
+
+# US2/computepm
 #export COMPUTE_ENDPOINT=https://api-z64.compute.us6.oraclecloud.com
-export STORAGE_AUTH_ENDPOINT=https://$IDENTITYDOMAIN.storage.oraclecloud.com/auth/v1.0
-export STORAGE_ENDPOINT=https://$IDENTITYDOMAIN.storage.oraclecloud.com/v1/Storage-$IDENTITYDOMAIN
+#export STORAGE_AUTH_ENDPOINT=https://$IDENTITYDOMAIN.storage.oraclecloud.com/auth/v1.0
+#export STORAGE_ENDPOINT=https://$IDENTITYDOMAIN.storage.oraclecloud.com/v1/Storage-$IDENTITYDOMAIN
 
 # Proxy setting (if needed)
-#export HTTPS_PROXY=https://www-proxy.us.oracle.com:80
-#export HTTP_PROXY=http://www-proxy.us.oracle.com:80
-#export http_proxy=http://www-proxy.us.oracle.com:80
-#export https_proxy=https://www-proxy.us.oracle.com:80
+export HTTPS_PROXY=https://www-proxy.us.oracle.com:80
+export HTTP_PROXY=http://www-proxy.us.oracle.com:80
+export http_proxy=http://www-proxy.us.oracle.com:80
+export https_proxy=https://www-proxy.us.oracle.com:80
