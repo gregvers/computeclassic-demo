870738b95fb9d9795fb8e375aa1e599d770c5453
gregvers <gregvers@gmail.com>
2017-12-21 15:23:18 -0800

    update

---
+++ env_template.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# account and user
+export IDENTITYDOMAIN=xxxxxxx
+export USER=xxxxxx@domain.com
+export PASSWORD="xxxxxx"
+
+# API endpoints
+export COMPUTE_ENDPOINT=https://xxxxxxxxxxxx
+export STORAGE_AUTH_ENDPOINT=https://$IDENTITYDOMAIN.xxxxxxxxxxx/auth/v1.0
+export STORAGE_ENDPOINT=https://$IDENTITYDOMAIN.xxxxxxxxxxxxxxxx/v1/Storage-$IDENTITYDOMAIN
+
